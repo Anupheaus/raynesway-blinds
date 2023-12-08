87f5efcd6ea10bf0be9f026a7fb45868626136a0
Tony Hales <tony.hales@revlifter.com>
2023-12-08 09:06:11 +0000

    Hopefully fixes for google SEO

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 
-window.addEventListener('load', () => {
-  const rootElement = document.getElementsByTagName('root').item(0);
-  if (rootElement == null) throw new Error('Root element not found');
-  const root = createRoot(rootElement);
-  root.render(<App />);
-});
+
+const rootElement = document.getElementsByTagName('root').item(0);
+if (rootElement == null) throw new Error('Root element not found');
+const root = createRoot(rootElement);
+root.render(<App />);
