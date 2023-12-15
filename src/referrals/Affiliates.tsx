0918ec74f4ded6e991a216353f9d3638861ed3ab
Tony Hales <tony.hales@revlifter.com>
2023-12-15 10:16:19 +0000

    Refactors the referral scheme terms and conditions in the Affiliates and Recommendations components. It updates the styling and adds a Typography component for better readability.

--- src/referrals/Affiliates.tsx
+++ src/referrals/Affiliates.tsx
@@ -14,6 +14,9 @@ const useStyles = createStyles({
     overflowY: 'auto',
     ...theme.scrollbars.styling,
   },
+  buttonArea: {
+    paddingBottom: 24,
+  },
 });
 
 interface Props {
@@ -40,19 +43,19 @@ export const Affiliates = createComponent('Affiliates', ({
             You simply hand out our postcard to your customer.&nbsp; If they go on to place an order with us and quote your unique reference number, we will give
             you 5% of the order value as a thank you.&nbsp; The postcards are sent to you free of charge and the number of them will be based on the number of potential
             customers you will be handing them to per month.&nbsp; You will be contacted regularly to ensure you always have enough postcards.
+            <ul>
+              <li>You will receive your reward at the end of the month following the month that the order was paid for in full, after any discounts or refunds have been applied.</li>
+              <li>You must be over 18, have a registered UK business and have a UK bank account to receive your reward.</li>
+              <li>You cannot recommend yourself - nice try though!</li>
+              <li>You cannot recommend anyone that lives at the same address as you.</li>
+              <li>There is no limit to the number of customers you can recommend.</li>
+              <li>You agree that your business may appear on our website or social media page(s) to advertise our affiliation while you are a member of the scheme.</li>
+              <li>We reserve the right to withdraw this offer at any time.</li>
+              <li>We reserve the right to refuse to pay a reward if we believe that the recommendation was not genuine or is in some way abusing our scheme.</li>
+              <li>We reserve the right to modify or amend these terms and conditions at any time.&nbsp; Any amendments will be available here on the website.</li>
+            </ul>
           </Typography>
-          <ul>
-            <li>You will receive your reward at the end of the month following the month that the order was paid for in full, after any discounts or refunds have been applied.</li>
-            <li>You must be over 18, have a registered UK business and have a UK bank account to receive your reward.</li>
-            <li>You cannot recommend yourself - nice try though!</li>
-            <li>You cannot recommend anyone that lives at the same address as you.</li>
-            <li>There is no limit to the number of customers you can recommend.</li>
-            <li>You agree that your business may appear on our website or social media page(s) to advertise our affiliation while you are a member of the scheme.</li>
-            <li>We reserve the right to withdraw this offer at any time.</li>
-            <li>We reserve the right to refuse to pay a reward if we believe that the recommendation was not genuine or is in some way abusing our scheme.</li>
-            <li>We reserve the right to modify or amend these terms and conditions at any time.&nbsp; Any amendments will be available here on the website.</li>
-          </ul>
-          <Flex alignCentrally>
+          <Flex alignCentrally className={css.buttonArea}>
             <Button onSelect={signUp}>Sign Up Here</Button>
           </Flex>
         </GridCell>
--- src/referrals/Recommendations.tsx
+++ src/referrals/Recommendations.tsx
@@ -4,6 +4,7 @@ import { referralsSubMenuOptions } from './ReferralsSubMenuOptions';
 import { Helmet } from 'react-helmet';
 import { Grid, GridCell } from '../grid';
 import { theme } from '../theme';
+import { Typography } from '../typography';
 
 const useStyles = createStyles({
   recommendations: {
@@ -31,18 +32,20 @@ export const Recommendations = createComponent('Recommendations', ({
       </Helmet>}
       <Grid cellHeight={300} gap={32}>
         <GridCell type="mediaAndText" title="Recommendations Scheme Terms and Conditions" mediaSrc="/images/recommendation.png" variant="horizontal" mediaMaxWidth={500}>
-          Simply put, you recommend a friend to us and tell them to make sure they mention you when contacting us.&nbsp; If they go on to place an order with us, we will give
-          you 5% of the order value as a thank you.<br />
-          <ul>
-            <li>You will receive your reward around one month after the order has been paid for in full, after any discounts or refunds have been applied.</li>
-            <li>You must be over 18 and have a UK bank account to receive your reward.</li>
-            <li>You cannot recommend yourself - nice try though!</li>
-            <li>You cannot recommend anyone that lives at the same address as you.</li>
-            <li>There is no limit to the number of friends you can recommend.</li>
-            <li>We reserve the right to withdraw this offer at any time.</li>
-            <li>We reserve the right to refuse to pay a reward if we believe that the recommendation was not genuine.</li>
-            <li>We reserve the right to modify or amend these terms and conditions at any time.&nbsp; Any amendments will be available here on the website.</li>
-          </ul>
+          <Typography type="paragraph">
+            Simply put, you recommend a friend to us and tell them to make sure they mention you when contacting us.&nbsp; If they go on to place an order with us, we will give
+            you 5% of the order value as a thank you.<br />
+            <ul>
+              <li>You will receive your reward around one month after the order has been paid for in full, after any discounts or refunds have been applied.</li>
+              <li>You must be over 18 and have a UK bank account to receive your reward.</li>
+              <li>You cannot recommend yourself - nice try though!</li>
+              <li>You cannot recommend anyone that lives at the same address as you.</li>
+              <li>There is no limit to the number of friends you can recommend.</li>
+              <li>We reserve the right to withdraw this offer at any time.</li>
+              <li>We reserve the right to refuse to pay a reward if we believe that the recommendation was not genuine.</li>
+              <li>We reserve the right to modify or amend these terms and conditions at any time.&nbsp; Any amendments will be available here on the website.</li>
+            </ul>
+          </Typography>
         </GridCell>
       </Grid>
     </Flex>
