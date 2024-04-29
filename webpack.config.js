6ca1b550b292c52f71a069aacf3f2d9d93cebf55
Tony Hales <tony.hales@revlifter.com>
2024-04-29 01:03:28 +0100

    Hopefully a fix to make the function use an email signature.

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,83 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ProgressPlugin = require('progress-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
+const createCommonSettings = isDev => ({
+  module: {
+    rules: [{
+      resourceQuery: /raw/,
+      type: 'asset/source',
+    }, {
+      test: /\.tsx?$/,
+      loader: 'ts-loader',
+      options: {
+        onlyCompileBundledFiles: true,
+        compilerOptions: {
+          declaration: false,
+          noEmit: false,
+        },
+      },
+    }],
+  },
+  ...(isDev ? {} : {
+    optimization: {
+      usedExports: true,
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          parallel: true,
+          terserOptions: {
+            keep_classnames: true,
+            keep_fnames: true,
+            sourceMap: false,
+          },
+        }),
+      ],
+    },
+  }),
+  plugins: [
+    ...(isDev ? [new ProgressPlugin()] : []),
+  ],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  stats: {
+    assets: false,
+    cached: false,
+    cachedAssets: false,
+    children: false,
+    chunks: false,
+    chunkGroups: false,
+    chunkModules: false,
+    chunkOrigins: false,
+    colors: true,
+    depth: false,
+    entrypoints: false,
+    env: false,
+    errors: true,
+    errorDetails: true,
+    hash: false,
+    logging: 'error',
+    modules: false,
+    outputPath: false,
+    performance: true,
+    providedExports: false,
+    publicPath: false,
+    reasons: false,
+    source: false,
+    timings: true,
+    usedExports: false,
+    version: true,
+    warnings: true,
+  },
+});
+
 module.exports = (env, argv) => {
   const isDev = argv.mode === 'development';
-  return {
+  const commonSettings = createCommonSettings(isDev);
+
+  return [{
+    ...commonSettings,
+    name: 'web',
     entry: { index: './src/index.tsx' },
     devtool: 'source-map',
     target: 'web',
@@ -13,39 +87,8 @@ module.exports = (env, argv) => {
       path: path.resolve(__dirname, './public'),
       publicPath: '/',
     },
-    module: {
-      rules: [{
-        resourceQuery: /raw/,
-        type: 'asset/source',
-      }, {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
-        options: {
-          onlyCompileBundledFiles: true,
-          compilerOptions: {
-            declaration: false,
-            noEmit: false,
-          },
-        },
-      }],
-    },
-    ...(isDev ? {} : {
-      optimization: {
-        usedExports: true,
-        minimize: true,
-        minimizer: [
-          new TerserPlugin({
-            parallel: true,
-            terserOptions: {
-              keep_classnames: true,
-              keep_fnames: true,
-              sourceMap: false,
-            },
-          }),
-        ],
-      },
-    }),
     plugins: [
+      ...commonSettings.plugins,
       new HtmlWebpackPlugin({
         template: path.join(__dirname, './public/index.html'),
         filename: 'index.html',
@@ -55,10 +98,9 @@ module.exports = (env, argv) => {
         cache: false,
         hash: true,
       }),
-      new ProgressPlugin(),
     ],
     resolve: {
-      extensions: ['.ts', '.tsx', '.js', '.jsx'],
+      ...commonSettings.resolve,
       alias: {
         'react': path.join(__dirname, './node_modules/react'),
         'react-dom': path.join(__dirname, './node_modules/react-dom'),
@@ -76,35 +118,15 @@ module.exports = (env, argv) => {
       port: 9000,
       historyApiFallback: true,
     },
-    stats: {
-      assets: false,
-      cached: false,
-      cachedAssets: false,
-      children: false,
-      chunks: false,
-      chunkGroups: false,
-      chunkModules: false,
-      chunkOrigins: false,
-      colors: true,
-      depth: false,
-      entrypoints: false,
-      env: false,
-      errors: true,
-      errorDetails: true,
-      hash: false,
-      logging: 'error',
-      modules: false,
-      outputPath: false,
-      performance: true,
-      providedExports: false,
-      publicPath: false,
-      reasons: false,
-      source: false,
-      timings: true,
-      usedExports: false,
-      version: true,
-      warnings: true,
+  }, {
+    ...commonSettings,
+    name: 'contact-us',
+    entry: { 'contact-us': './functions/api/contact-us.ts' },
+    devtool: 'source-map',
+    target: 'node',
+    output: {
+      path: path.resolve(__dirname, './functions/api'),
     },
-  };
+  }];
 };
 
